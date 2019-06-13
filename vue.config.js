cddc5678661769850cbea6d5a1b5853ba851a246
周林 <634023699@qq.com>
2019-06-13 09:45:23 +0800

    添加项目配置文件

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // 选项...
+    devServer: {
+        port: 3000, // 端口号
+        proxy: {
+            '/api': {
+                target: 'http://www.havalfl.com',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/apis': '' //需要rewrite的,
+                }
+            },
+        },
+
+    },
+};
